09620ebd15cc16062043aa037bbdf7bd73c19a47
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-08-15 20:49:07 -0700

    Update DAbot.py

--- DAbot.py
+++ DAbot.py
@@ -1,36 +1,24 @@
 import streamlit as st
 import pandas as pd
 import ydata_profiling as yp
+from streamlit_pandas_profiling import st_profile_report
 
-# Set up Streamlit app
 st.title("Data Analyst Bot")
 
-# Initialize variables
 data_uploaded = False
 eda_done = False
 df = None
 
-# Page 1: Upload Dataset
-st.header("Step 1: Upload Dataset")
-st.info("Please upload your dataset in CSV format.")
+st.header("Please upload your dataset")
 file = st.file_uploader("Upload CSV File", type=["csv"])
-
-if file is not None:
-    df = pd.read_csv(file)
-    st.success("Dataset uploaded successfully!")
-    data_uploaded = True
+if file: 
+    df = pd.read_csv(file, index_col=None)
+    df.to_csv('dataset.csv', index=None)
+    st.dataframe(df)
 
 if data_uploaded:
-    # Page 2: Exploratory Data Analysis (EDA)
-    st.header("Step 2: Exploratory Data Analysis (EDA)")
-    st.write("Explore your dataset and generate a data report.")
+    st.header("Exploratory Data Analysis (EDA)")
 
     # Perform EDA using ydata_profiling
     profile_df = yp.ProfileReport(df)
-    
-    # Display EDA report
-    st.write(profile_df.to_html(), unsafe_allow_html=True)
-    eda_done = True
-
-# Add a footer to the app
-st.text("Bravish @ 2023")
+    st_profile_report(profile_df)
